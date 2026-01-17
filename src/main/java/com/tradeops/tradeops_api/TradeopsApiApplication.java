622aab2e8031f8c657ded668fb34af54793235fc
cshields164 <cshields164@gmail.com>
2026-01-16 19:26:11 -0500

    chore: init spring boot project

---
+++ src/main/java/com/tradeops/tradeops_api/TradeopsApiApplication.java
@@ -0,0 +1,13 @@
+package com.tradeops.tradeops_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeopsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeopsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradeops/tradeops_api/TradeopsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradeops.tradeops_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeopsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
